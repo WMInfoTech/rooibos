4573110d602b8a458c07c280994c522ad6652463
Jenn Wheeler <jawheeler@wm.edu>
2021-01-11 16:33:06 -0500

    remove env stuff from start script and from supervisord configuration

--- docker/start.sh
+++ docker/start.sh
@@ -1,13 +1,5 @@
 #!/bin/sh
 
-# make sure db environment variables get picked up by suprvisord for workers
-echo 'export DB_HOST='$DB_HOST > /etc/default/supervisor
-echo 'export DB_USER='$DB_USER >> /etc/default/supervisor
-echo 'export DB_PASSWORD='$DB_PASSWORD >> /etc/default/supervisor
-echo 'export DB_NAME='$DB_NAME >> /etc/default/supervisor
-echo 'export CELERY_BROKER_URL='$CELERY_BROKER_URL >> /etc/default/supervisor
-echo 'export SOLR_URL='$SOLR_URL >> /etc/default/supervisor
-
 service supervisor start
 
 gunicorn -b 0.0.0.0:8080 rooibos.wsgi:application --log-config docker/gunicorn-logging.conf
